bdf3771c6d9bc20ce33f567f2f173ac83682dc78
Aye Chan <ayechan.sant@gmail.com>
2023-02-24 13:58:16 -0500

    js code

---
+++ library.js
@@ -0,0 +1,14 @@
+const library = [];
+
+function Book(title, author, pages) {
+  // this makes the object thats it
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+}
+
+function addBook() {
+  library.push(book);
+}
+
+console.log('hello');
