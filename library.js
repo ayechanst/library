a3b29b1f3ad95c5150bcd01cf3169abad7122283
Aye Chan <ayechan.sant@gmail.com>
2023-03-01 09:45:46 -0700

    added a class

--- library.js
+++ library.js
@@ -1,14 +1,30 @@
-const library = [];
+const form = document.getElementById('form');
+let titleInput = document.getElementById('title');
+let authorInput = document.getElementById('author');
+let pagesInput = document.getElementById('pages');
 
-function Book(title, author, pages) {
-  // this makes the object thats it
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
+const library = [
+  { title: 'Moby Cock', author: 'Bobby Bob', pages: 420 },
+  { title: 'Tom Sawyer', author: 'Mark Twain', pages: 432343 },
+  { title: 'Beyond Good and Evil', author: 'Nichze', pages: 2 },
+];
+
+class Book {
+  constructor(title, author, pages) {
+    // this makes the object thats it
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    return {};
+  }
 }
+
+form.addEventListener('submit', () => {
+  Book(title, author, pages);
+});
 
 function addBook() {
   library.push(book);
 }
 
-console.log('hello');
+console.log(library);
