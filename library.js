bca07570248576e082fe9be87c82dbf1931c2894
Aye Chan <ayechan.sant@gmail.com>
2023-03-02 17:12:37 -0700

    make book function

--- library.js
+++ library.js
@@ -1,7 +1,4 @@
-const form = document.getElementById('form');
-let titleInput = document.getElementById('title');
-let authorInput = document.getElementById('author');
-let pagesInput = document.getElementById('pages');
+const button = document.getElementById('button');
 
 const library = [
   { title: 'Moby Cock', author: 'Bobby Bob', pages: 420 },
@@ -9,22 +6,60 @@ const library = [
   { title: 'Beyond Good and Evil', author: 'Nichze', pages: 2 },
 ];
 
+function makeBook(book) {
+  // make this function set innerHTML to the correct book values (only make one book a time)
+  const bookShelf = document.getElementById('bookshelf');
+  const bookDiv = document.createElement('div');
+  // set the id to the value of the title
+  let bookTitle = document.createElement('p');
+  bookTitle = book.title;
+  bookTitle.innerHTML = book.title;
+  bookDiv.appendChild(bookTitle);
+  // done book title
+  let bookAuthor = document.createElement('p');
+  bookAuthor = book.author;
+  bookAuthor.innerHTML = book.author;
+  bookDiv.appendChild(bookAuthor);
+  // done book author
+  let bookPages = document.createElement('p');
+  bookPages = book.author;
+  bookPages.innerHTML = book.pages;
+  bookDiv.appendChild(bookPages);
+  // done book pages
+  bookShelf.appendChild(bookDiv);
+}
+
 class Book {
   constructor(title, author, pages) {
     // this makes the object thats it
     this.title = title;
     this.author = author;
     this.pages = pages;
-    return {};
   }
 }
 
-form.addEventListener('submit', () => {
-  Book(title, author, pages);
+// end helper functions
+
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+  const titleInput = document.getElementById('title').value;
+  const authorInput = document.getElementById('author').value;
+  const pagesInput = document.getElementById('pages').value;
+  const book = new Book(titleInput, authorInput, pagesInput);
+  library.push(book);
+  library.forEach((item) => {
+    makeBook(item);
+  });
+  makeBook(book);
+  console.log(authorInput);
+  console.log(pagesInput);
+  console.log(pagesInput);
 });
 
-function addBook() {
-  library.push(book);
-}
+// make a thing that makes a card
 
+// make a thing that creates sends the card to the html
+// use the elemnt
+
+// eslint-disable-next-line no-console
 console.log(library);
